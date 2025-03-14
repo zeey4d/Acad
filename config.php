94a03bbdbfb495260aa606035cfd7e11b406db7c
Ali Abdulsalam <aiyabd739252434@gmail.com>
2025-03-15 01:15:42 +0300

    edit in conflg.php

--- config.php
+++ config.php
@@ -7,6 +7,11 @@ return [
         "port" => "3306",
         "dbname" => "badir",
         "charset" => "utf8mb4",
+        "username" => "root",
+        "password" => "aliabd739252434"
+        
+        
+
     ],
     "phpmailer" => [
         "host" => "smtp.mailtrap.io",
