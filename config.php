2bc48dd850af62a6908c12086b3bfd56ea625c28
Ali Abdulsalam <aiyabd739252434@gmail.com>
2025-03-15 17:27:04 +0300

    do

--- config.php
+++ config.php
@@ -8,11 +8,8 @@ return [
         "dbname" => "badir",
         "charset" => "utf8mb4",
         "username"=>"root",
-<<<<<<< HEAD
+
         "password"=>"aliabd739252434"
-=======
-        "password"=>""
->>>>>>> c6d2bbb8d46a4152ab547efe1fa28f4f54c456fc
     ],
     
 
