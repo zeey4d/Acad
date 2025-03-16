13f3daed65e06342d5f7a0fba91e7b2660aa9834
ahmed anwar <ahmedgazy375@gimal.com>
2025-03-16 22:13:42 +0300

    fix router not loding css

--- controllers/cards/index.php
+++ controllers/cards/index.php
@@ -1,14 +1,20 @@
 <?php
-use core\App ;
-use core\Database ;
-$db = App::resolve(Database::class);
+// use core\App ;
+// use core\Database ;
+// $db = App::resolve(Database::class);
 
 
-$heading = "All My tests";
+// $heading = "All My cards";
 
 
-// $cards = $db->query("SELECT * from cards ;")->fetchAll();
+// // $cards = $db->query("SELECT * from cards ;")->fetchAll();
 
+
+// require "views/pages/index_view.php";
+
+$heading = "Home";
+
+$_SESSION['name']= "Developer";
 
 require "views/pages/cards/index_view.php";
 
