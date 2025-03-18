47fc54812908ba797f038115328a01aede336fd5
gamal729 <gam737344143@gmail.com>
2025-03-18 22:20:39 +0300

    do

--- controllers/items/index.php
+++ controllers/items/index.php
@@ -7,7 +7,7 @@ $db = App::resolve(Database::class);
 $heading = "All My tests";
 
 
-// $items = $db->query("SELECT * from items ;")->fetchAll();
+$items = $db->query("SELECT * from items ;")->fetchAll();
 
 
 require "views/pages/items/index_view.php";
