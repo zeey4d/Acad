733cfd5489df4077154213992e5fa6503c1ac6f5
MubarakAshrafAlrawy <mubarakashrafalrawy@gmail.com>
2025-03-18 23:17:02 +0300

    update queries and add some queries to some several files

--- controllers/charity_projects/create.php
+++ controllers/charity_projects/create.php
@@ -4,9 +4,9 @@ $heading = "Create test";
 
 
 
-$db->query("INSERT INTO projects ( partner_id,  category_id,  level,  name,  photo,  short_description,  full_description,  type,  cost,  start_at,  end_at,  state,  directorate)
+$partner_id = $db->query("INSERT INTO projects ( category_id,  level,  name,  photo,  short_description,  full_description,  type,  cost,  start_at,  end_at,  state,  directorate)
 VALUES
-    (    :partner_id,
+    (
     :category_id,
     :level,
     :name,
@@ -14,25 +14,23 @@ VALUES
     :short_description,
     :full_description,
     :type, :cost,
-    :start_at,
+    now(),
     :end_at,
     :state,
     :directorate
-    )", 
+    ) RETURNING partner_id",
     [
-        'partner_id' => $_POST['partner_id'],
         'category_id'=> $_POST['category_id'],
-        'name' => $_POST['name'], 
-        'photo' => $_POST['photo'], 
-        'level' => $_POST['level'], 
-        'short_description' => $_POST['short_description'], 
-        'full_description' => $_POST['full_description'], 
-        'type' => $_POST['type'], 
-        'cost' => $_POST['cost'], 
-        'start_at' => $_POST['start_at'], 
-        'end_at' => $_POST['end_at'], 
-        'state' => $_POST['state'], 
+        'name' => $_POST['name'],
+        'photo' => $_POST['photo'],
+        'level' => $_POST['level'],
+        'short_description' => $_POST['short_description'],
+        'full_description' => $_POST['full_description'],
+        'type' => $_POST['type'],
+        'cost' => $_POST['cost'],
+        'end_at' => $_POST['end_at'],
+        'state' => $_POST['state'],
         'directorate' => $_POST['directorate']
-    ]);
+    ])->getGeneratedKey('partner_id');
 
 require "views/pages/charity_projects/create_view.php";
--- controllers/charity_projects/lest.php
+++ controllers/charity_projects/lest.php
@@ -18,6 +18,17 @@ $userID = 1;
 
 //authorize($note['other_id'] == $userID);
 
-
+$projects = $db->query("SELECT A.project_id, A.partner_id, A.category_id, A.level, A.name, A.photo, A.short_description, A.full_description, A.type, A.cost, sum(B.cost) as collected_money, A.start_at, A.end_at, A.state, A.directorate, MAX(B.donate_date) as donste_date
+From projects A join users_donate_projects B on (A.project_id = B.project_id)
+group by A.project_id
+having A.project_id in
+		(
+			select project_id 
+            from users_donate_projects 
+            where user_id =  :USER_ID
+		)
+order by (donate_date) desc;",[
+    'USER_ID' => $userID
+])->fetchAll();
 
 require "views/pages/charity_campaigns/show_view.php";
