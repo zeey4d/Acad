8bc6a8213589acebc91ccfae33a5e2fa1e1f3461
ahmed anwar <ahmedgazy375@gimal.com>
2025-03-19 22:25:53 +0300

    fix stor contrloller

--- controllers/charity_campaigns/create.php
+++ controllers/charity_campaigns/create.php
@@ -7,19 +7,19 @@ $heading = "Create test";
 // $db->query("INSERT INTO charity_campaigns (name) VALUES (':item')", [
 //     'item' => $_POST['item'],
 //   ]);
-$campaign_id = $db->query
-  ("INSERT INTO campaigns (category_id, partner_id, campaign_request_id, name, short_description, full_description, cost, state, start_at, end_at)
-  VALUES (:category_id, :partner_id, :campaign_request_id, :name, :short_description, :full_description, :cost, :state, now(), :end_at) RETURNING campaign_id",
-  [
-      'category_id' => $_POST['category_id'],
-      'partner_id' => $_POST['partner_id'],
-      'campaign_request_id' => $_POST['campaign_request_id'],
-      'name' => $_POST['name'],
-      'short_description' => $_POST['short_description'],
-      'full_description' => $_POST['full_description'],
-      'cost' => $_POST['cost'],
-      'state' => $_POST['state'],
-      'end_at' => $_POST['end_at'],
-  ])->getGeneratedKey('campaign_id');
+// $campaign_id = $db->query
+//   ("INSERT INTO campaigns (category_id, partner_id, campaign_request_id, name, short_description, full_description, cost, state, start_at, end_at)
+//   VALUES (:category_id, :partner_id, :campaign_request_id, :name, :short_description, :full_description, :cost, :state, now(), :end_at) RETURNING campaign_id",
+//   [
+//       'category_id' => $_POST['category_id'],
+//       'partner_id' => $_POST['partner_id'],
+//       'campaign_request_id' => $_POST['campaign_request_id'],
+//       'name' => $_POST['name'],
+//       'short_description' => $_POST['short_description'],
+//       'full_description' => $_POST['full_description'],
+//       'cost' => $_POST['cost'],
+//       'state' => $_POST['state'],
+//       'end_at' => $_POST['end_at'],
+//   ])->getGeneratedKey('campaign_id');
 
 require "views/pages/charity_campaigns/create_view.php";
--- controllers/charity_projects/create.php
+++ controllers/charity_projects/create.php
@@ -4,33 +4,33 @@ $heading = "Create test";
 
 
 
-$partner_id = $db->query("INSERT INTO projects ( category_id,  level,  name,  photo,  short_description,  full_description,  type,  cost,  start_at,  end_at,  state,  directorate)
-VALUES
-    (
-    :category_id,
-    :level,
-    :name,
-    :photo,
-    :short_description,
-    :full_description,
-    :type, :cost,
-    now(),
-    :end_at,
-    :state,
-    :directorate
-    ) RETURNING partner_id",
-    [
-        'category_id'=> $_POST['category_id'],
-        'name' => $_POST['name'],
-        'photo' => $_POST['photo'],
-        'level' => $_POST['level'],
-        'short_description' => $_POST['short_description'],
-        'full_description' => $_POST['full_description'],
-        'type' => $_POST['type'],
-        'cost' => $_POST['cost'],
-        'end_at' => $_POST['end_at'],
-        'state' => $_POST['state'],
-        'directorate' => $_POST['directorate']
-    ])->getGeneratedKey('partner_id');
+// $partner_id = $db->query("INSERT INTO projects ( category_id,  level,  name,  photo,  short_description,  full_description,  type,  cost,  start_at,  end_at,  state,  directorate)
+// VALUES
+//     (
+//     :category_id,
+//     :level,
+//     :name,
+//     :photo,
+//     :short_description,
+//     :full_description,
+//     :type, :cost,
+//     now(),
+//     :end_at,
+//     :state,
+//     :directorate
+//     ) RETURNING partner_id",
+//     [
+//         'category_id'=> $_POST['category_id'],
+//         'name' => $_POST['name'],
+//         'photo' => $_POST['photo'],
+//         'level' => $_POST['level'],
+//         'short_description' => $_POST['short_description'],
+//         'full_description' => $_POST['full_description'],
+//         'type' => $_POST['type'],
+//         'cost' => $_POST['cost'],
+//         'end_at' => $_POST['end_at'],
+//         'state' => $_POST['state'],
+//         'directorate' => $_POST['directorate']
+//     ])->getGeneratedKey('partner_id');
 
 require "views/pages/charity_projects/create_view.php";
