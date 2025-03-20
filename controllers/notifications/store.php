abdfe38930b267e895fc21e1f6158754f49fed06
MubarakAshrafAlrawy <mubarakashrafalrawy@gmail.com>
2025-03-20 23:00:23 +0300

    adding the rest queries

--- controllers/notifications/store.php
+++ controllers/notifications/store.php
@@ -49,29 +49,59 @@ foreach($_POST['projects'] as $project){
     $db->query(
         "INSERT INTO L_projects_notifications (notification_id, project_id) VALUES (:notification_id, :project_id)",[
             'notification_id' => $notification_id,
-            'project_id' => $project,
+            'project_id' => $project['project_id'],
         ]
     );
+    $users = $db->query("SELECT user_id FROM users_noti_projects WHERE project_id = :project_id",[
+        'project_id'=>$project['project_id']
+    ])->fetchAll();
+    foreach($users as $user){
+        $db->query(
+            "INSERT into users_notification (notification_id,user_id) values (:notification_id,:user_id)",[
+                'notification_id' => $notification_id,
+                'user_id' => $user;
+            ]
+        );
+    }
 }
 foreach($_POST['endowments'] as $endowment){
     $db->query(
         "INSERT INTO L_endowments_notifications (notification_id, endowment_id) VALUES (:notification_id, :endowment_id)",[
             'notification_id' => $notification_id,
-            'endowment_id' => $endowment,
+            'endowment_id' => $endowment['endowment_id'],
         ]
     );
+    $users = $db->query("SELECT user_id FROM users_noti_endwoements WHERE endwoement_id = :endwoement_id",[
+        'endwoement_id'=>$endwoement['endwoement_id']
+    ])->fetchAll();
+    foreach($users as $user){
+        $db->query(
+            "INSERT into users_notification (notification_id,user_id) values (:notification_id,:user_id)",[
+                'notification_id' => $notification_id,
+                'user_id' => $user;
+            ]
+        );
+    }
 }
 foreach($_POST['campaigns'] as $campaign){
     $db->query(
         "INSERT INTO L_campaigns_notifications (notification_id, campaign_id) VALUES (:notification_id, :campaign_id)",[
             'notification_id' => $notification_id,
-            'campaign_id' => $campaign,
+            'campaign_id' => $campaign['campaign_id'],
         ]
     );
+    $users = $db->query("SELECT user_id FROM users_noti_campaigns WHERE campaign_id = :campaign_id",[
+        'campaign_id'=>$campaign['campaign_id']
+    ])->fetchAll();// المستخدمين الذين فعلوا الاشعارات لهذه الحملة
+    foreach($users as $user){
+        $db->query(
+            "INSERT into users_notification (notification_id,user_id) values (:notification_id,:user_id)",[
+                'notification_id' => $notification_id,
+                'user_id' => $user;
+            ]
+        );// ارسال الاشعارات لهم
+    }
 }
-$users = $db->query(
-    "SELECT A.user_id FROM users_noti_campaigns "
-)
 header("Location: /pages/notifications");
 die();
 
