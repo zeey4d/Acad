0b8471b42d367fec49571724af3d2c1a35534943
ahmed anwar <ahmedgazy375@gimal.com>
2025-03-21 06:22:33 +0300

    sync

--- vendor/sebastian/version/src/Version.php
+++ vendor/sebastian/version/src/Version.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 /*
  * This file is part of sebastian/version.
  *
@@ -7,6 +9,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 namespace SebastianBergmann;
 
 use function end;
@@ -20,7 +23,7 @@ use function stream_get_contents;
 use function substr_count;
 use function trim;
 
-final readonly class Version
+final class Version
 {
     /**
      * @var non-empty-string
