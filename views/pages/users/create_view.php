9f64a62c010b1bd47d82ae4dd6c71b4457d15497
Ali Abdulsalam <aiyabd739252434@gmail.com>
2025-03-21 05:56:05 +0300

    Authorizatoin and validatoin

--- views/pages/users/create_view.php
+++ views/pages/users/create_view.php
@@ -8,22 +8,6 @@
   <!-- انشاء حساب جديد -->
   <section class="form_signup" a>
     
-<<<<<<< HEAD
-    <div class="create" >
-      <h1>إنشاءحساب</h1>
-      <form action="/uders_store" method="post">
-        <label for="name">:الإسم</label><br>
-        <input id="name" type="text" name="name" placeholder="الإسم"><br>
-        <label for="email">:البريدالإلكتروني</label><br>
-        <input id="email" type="email" name="email" placeholder="البريدالإلكتروني"><br>
-        <label for="password">:كلمةالمرور</label><br>
-        <input id="password" type="password" name="password" placeholder="كلمةالمرور"><br>
-        <label for="confirm_password">:تأكيدكلمةالمرور</label><br>
-        <input id="confirm_password" type="password" name="confirm_password" placeholder="تأكيدكلمةالمرور"><br>
-        <label for="phone_number">:رقم الهاتف</label><br>
-        <input id="phone_number" type="text" name="phone_number" placeholder="رقم الهاتف"><br>
-        <label for="place">:المنطقة</label><br>
-=======
     <div class="create">
       <div>
       <h1>إنشاءحساب</h1>
@@ -39,7 +23,6 @@
         <label for="phone_number">:رقم الهاتف</label>
         <input id="phone_number" type="text" name="phone_number" placeholder="رقم الهاتف">
         <label for="place">:المنطقة</label>
->>>>>>> ff1748cd1b6b7255a5985a99354128cee49096ca
         <input id="place" type="text" name="place" placeholder="المنطقة"><br>
         <button class="button" id="button">تسجيل الدخول</button>
 
