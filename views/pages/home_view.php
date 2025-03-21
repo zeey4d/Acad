0f1d237cee46477ea3ba05dc5588e25dfae3a3c6
ahmed anwar <ahmedgazy375@gimal.com>
2025-03-21 07:27:17 +0300

    sync

--- views/pages/home_view.php
+++ views/pages/home_view.php
@@ -60,40 +60,6 @@ echo"<br><br><br>" ;
       </section>
     </main>
 
-    <main class="main_cart">
-      <section class="container">
-        <?php foreach($campaigns as $campaign): ?>
-        <div class="donation-card">
-          <img src="views/media/images/<?= htmlspecialchars($campaign['photo'] ?? "11.png") ?>" alt="مشروع نور السعودية">
-          <div class="donation-info">
-            <div class="aghtha">
-              <h6>إغاثة</h6>
-              <h5>رقم الحملة : <?= htmlspecialchars($campaign['campaign_id']) ?></h5>
-              <a href=""><img src="" alt=""></a>
-            </div>
-            <h3><?= htmlspecialchars($campaign['name']) ?></h3>
-            <div class="progress-bar">
-              <div class="progress" style="width:<?= htmlspecialchars(($campaign['collected_money']/$campaign['cost'])*100) ?>% "></div>
-            </div>
-            <div class="donation-details">
-              <div>
-                <p><strong style="display: inline;">SR <?= htmlspecialchars($campaign['collected_money']) ?>/</strong><?= htmlspecialchars($campaign['cost']) ?></p>
-              </div>
-            </div>
-            <div class="donate-section">
-              <input class="inp" type="text" placeholder=" مبلغ التبرع                   ر.س">
-              <button class="donate-btn">تبرع الأن</button>
-              <button class="donate_cart"><img src="views/media/images/cart.png" alt=""></button>
-            </div>
-            <div class="details">عرض التفاصيل</div>
-          </div>
-        </div>
-          <?php endforeach; ?>
-      </section>
-      <section class="bar_action">
-
-      </section>
-    </main>
 
   </section>
   <!-- الاحداث السريعه -->
