760a7bfa0f1632dd0cad7ae0305ff33157bfb245
Ziad <fisal191919@gmail.com>
2025-03-22 21:54:55 +0300

    ahmad

--- views/pages/charity_campaigns/index_view.php
+++ views/pages/charity_campaigns/index_view.php
@@ -5,47 +5,42 @@
 
 <?php //dd($campaigns) ?>
 
-<main>
-  <section class="bar_search">
-    <form action="" method="">
-      <input id="search" type="search" name="search" placeholder="Search">
-      <select name="project_type" id="project_type">
-        <option value="">نوع المشروع</option>
-        <option value="رجوع"> رجوع </option>
-        <option value="محتاجيين"> محتاجيين</option>
-        <option value="عمليات"> عمليات</option>
-        <option value="بناء مسكن"> بناء مسكن</option>
-      </select>
-      <select name="region" id="region">
-        <option value="">المنطقة</option>
-      </select>
 
-    </form>
-  
-  </section>
-  <h1>حملات التبرع المتاحة</h1>
+  <h1 style="  text-align: center;
+    color: var(--font-color-bh);
+    margin: var(--margin-xl);">حملات التبرع المتاحة</h1>
 
-  <section class="container">
-  <?php foreach($campaigns as $campaign): ?>
+  <section class="container_card">
+        <?php foreach($campaigns as $campaign): ?>
+        <div class="donation-card">
+          <img src="views/media/images/<?= htmlspecialchars($campaign['photo'] ?? "11.png") ?>" alt="مشروع نور السعودية" loading="lazy">
+          <div class="donation-info">
+            <div class="aghtha">
+              <h6>إغاثة</h6>
+              <h5>رقم الحملة : <?= htmlspecialchars($campaign['campaign_id']) ?></h5>
+              <a href=""><img src="" alt=""></a>
+            </div>
+            <h3><?= htmlspecialchars($campaign['name']) ?></h3>
+            <div class="progress-bar">
+              <div class="progress" style="width:<?= htmlspecialchars(($campaign['collected_money']/$campaign['cost'])*100) ?>% "></div>
+            </div>
+            <div class="donation-details">
+              <div>
+                <p><strong style="display: inline;">SR <?= htmlspecialchars($campaign['collected_money']) ?>/</strong><?= htmlspecialchars($campaign['cost']) ?></p>
+              </div>
+            </div>
+            <div class="donate-section">
+              <input class="inp" type="text" placeholder=" مبلغ التبرع                   ر.س">
+              <button class="donate-btn">تبرع الأن</button>
+              <button class="donate_cart"><img src="views/media/images/cart.png" alt=""></button>
+            </div>
+            <div class="details">عرض التفاصيل</div>
+          </div>
+        </div>
+          <?php endforeach; ?>
+      </section>
+      <section class="bar_action">
 
-    <div class="donation-card">
-      <img src="views/media/images/<?= htmlspecialchars($campaign['photo'] ?? "11.png") ?>" alt=" " loading="lazy">
-      <strong>بادر</strong>
-      <p><img src="" alt=""> <strong>رقم الحملة : <?= htmlspecialchars($campaign['campaign_id']) ?></strong></p>
-      <p><img src="" alt=""> <strong>المنطقة تعز</strong></p>
-      <h3>مشروع مساعدة المحتاجين</h3>
-      <p>
-        <span><?= htmlspecialchars(($campaign['collected_money']/$campaign['cost'])*100) ?>%</span>
-        <strong></strong>
-        <?= htmlspecialchars($campaign['cost']) ?>/<?= htmlspecialchars($campaign['collected_money']) ?> ريال
-      </p>
-
-
-      </form>
-      <a class="view_details" href="">عرض التفاصيل</a>
-      
-    </div>
-    <?php endforeach; ?>
-    </section>
+      </section>
 </main>
 <?php require('views/parts/footer.php') ?>
