2d1f1b1d57eb1e40a4e503eacd95c08320b94363
Ali Abdulsalam <aiyabd739252434@gmail.com>
2025-03-23 22:32:05 +0300

    do

--- controllers/charity_campaigns/store.php
+++ controllers/charity_campaigns/store.php
@@ -10,6 +10,14 @@ $db = App::resolve(Database::class);
 
 $errors = [];
 
+//if (!(Validator::string($_POST['name'], 1, 255))) {
+  //  $errors["name"] = "Titel  is too short or too long";
+//}
+// if (!(Validator::string($_POST['body'], 1, 1000))) {
+//     $errors["titel"] = " body is too short or long";
+// }
+
+
 if (! empty($errors)) {
     require "views/pages/charity_campaigns/create_view.php";
     die();
@@ -17,16 +25,14 @@ if (! empty($errors)) {
 
 
 $campaign_id = $db->query
-  ("INSERT INTO campaigns (category_id, partner_id, campaign_request_id, name, short_description, full_description, 
-  cost, state, start_at, end_at, photo)
-  VALUES (:category_id, :partner_id, :campaign_request_id, :name, :short_description, :full_description, 
-  :cost, :state ,now(), :end_at, :photo)",
+  ("INSERT INTO campaigns (category_id, partner_id, campaign_request_id, name, short_description, full_description, cost, state, start_at, end_at, photo)
+  VALUES (:category_id, :partner_id, :campaign_request_id, :name, :short_description, :full_description, :cost, :state ,now(), :end_at, :photo)",
   [
       'category_id' => $_POST['category_id'],
       'partner_id' => $_POST['partner_id'],
       'campaign_request_id' => $_POST['campaign_request_id'],
       'name' => $_POST['name'],
-      'short_description' => $_POST['short_description'],   
+      'short_description' => $_POST['short_description'],
       'full_description' => $_POST['full_description'],
       'cost' => $_POST['cost'],
       'state' => $_POST['state'],
