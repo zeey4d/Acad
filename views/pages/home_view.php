2ab38eb7bee0dde039c9b72dc3f6c1ddb5aeb7b8
Ziad <fisal191919@gmail.com>
2025-03-26 04:09:52 +0300

    style pages any create

--- views/pages/home_view.php
+++ views/pages/home_view.php
@@ -17,11 +17,8 @@
   <!-- الصوره الكبير الي بل البدايه -->
   <section class="hero">
     <img src="views/media/images/andrewSmall.jpg" alt="">
-<<<<<<< HEAD
     <h1  style="         font-size: var(--font-size-ll);  color : white;  margin: -110px 10px 0 0; text-align: right;">بِفَضْلِ تَبَرُّعاتِكُمْ، نَصْنَعُ فَرْقًا حَقِيقِيًّا فِي حَياةِ الْمُحْتاجِين</h1>
-=======
-    <h1 style=" color : white;  margin: -80px 10px 0 0; text-align: right;">بِفَضْلِ تَبَرُّعاتِكُمْ، نَصْنَعُ فَرْقًا حَقِيقِيًّا فِي حَياةِ الْمُحْتاجِين</h1>
->>>>>>> 722bffdcddfdc99100978c803025afc2132cd329
+
 
 
   </section>
@@ -82,16 +79,11 @@
 
   </section>
   <!-- الاحداث السريعه -->
-<<<<<<< HEAD
   <h1 style="background-color: white; color: var(--font-color-bh);text-align: center;         margin: var(--margin-xl); " >بِتَكاتُفِنا، نَسْتَطِيعُ تَحْقِيقَ الْمُسْتَحِيل </h1>
 
   <section  class="Fast-acting">
     <div class="tbr3">
-=======
-  <section class="Fast-acting">
-    <div class="tbr3">
-      <h1>بِتَكاتُفِنا، نَسْتَطِيعُ تَحْقِيقَ الْمُسْتَحِيل </h1>
->>>>>>> 722bffdcddfdc99100978c803025afc2132cd329
+
 
       <div class="t3">
         <p>بفضل كرمكم تجاوزنا الكثير من التبرعات. معاً نصنع الفرق.</p>
