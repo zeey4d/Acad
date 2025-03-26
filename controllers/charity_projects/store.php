914f51a56424bfd15c8249a1d44a43988920610b
Ali Abdulsalam <aiyabd739252434@gmail.com>
2025-03-26 17:44:49 +0300

    Authorizatoin and validatoin

--- controllers/charity_projects/store.php
+++ controllers/charity_projects/store.php
@@ -26,60 +26,58 @@ if (! empty($errors)) {
 // $db->query("INSERT INTO charity_projects (name) VALUES (:name)", [
 //     'name' => $_POST['name'],
 // ]);
-// $partner_id = $db->query("INSERT INTO projects ( partner_id,  category_id,  level,  name,  photo,  short_description,  full_description,  type,  cost,  start_at,  end_at,  state,  directorate)
-// VALUES
-//     (
-//     :partner_id,
-//     :category_id,
-//     :level,
-//     :name,
-//     :photo,
-//     :short_description,
-//     :full_description,
-//     :type, :cost,
-//     now(),
-//     :end_at,
-//     :state,
-//     :directorate
-//     )",
-//     [
-//         'partner_id' => $_POST['partner_id'],
-//         'category_id'=> $_POST['category_id'],
-//         'name' => $_POST['name'],
-//         'photo' => $_POST['photo'],
-//         'level' => $_POST['level'],
-//         'short_description' => $_POST['short_description'],
-//         'full_description' => $_POST['full_description'],
-//         'type' => $_POST['type'],
-//         'cost' => $_POST['cost'],
-//         'end_at' => $_POST['end_at'],
-//         'state' => $_POST['state'],
-//         'directorate' => $_POST['directorate']
-//     ])->getGeneratedKey('project_id');
 
+$partner_id =$db->query(
+    "INSERT INTO projects (
+        partner_id,
+        category_id,
+        name,
+        photo,
+        short_description,
+        full_description,
+        cost,
+        country,
+        city,
+        street
+    ) VALUES (
+        :partner_id,
+        :category_id,
+        :name,
+        :photo,
+        :short_description,
+        :full_description,
+        :cost,
+        :country,
+        :city,
+        :street
+    )",
+    [
+        'partner_id' => $_POST['partner_id'],
+        'category_id' => $_POST['category_id'],
+        'name' => htmlspecialchars($_POST['name']),
+        'photo' => $_POST['photo'],
+        'short_description' => htmlspecialchars($_POST['short_description']),
+        'full_description' => htmlspecialchars($_POST['full_description']),
+        'cost' => filter_var($_POST['cost'], FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION),
+        'country' => htmlspecialchars($_POST['country']),
+        'city' => htmlspecialchars($_POST['city']),
+        'street' => htmlspecialchars($_POST['street'])
+    ]
+);
 
 // استقبال البيانات المطابقة لقاعدة البيانات 
-// $category_id = $_POST['category_id'];
-// $partner_id = $_POST['partner_id'];
-// $name = $_POST['name'];
-// $short_description = $_POST['short_description'];
-// $full_description = $_POST['full_description'];
-// $cost = $_POST['cost'];
-// $state = $_POST['state'];
-// $end_at = $_POST['end_at'];
-// $age = $_POST ['age']; 
-
-//   // استقبال البيانات من النموذج
-//  $caseType = $_POST['caseType'];
-//  $age = $_POST ['age']; 
-//  $circumstances = $_POST['circumstances'];
-//  $amount = $_POST['amount'];
-//  $accountNumber = $_POST['accountNumber']; 
-//  $bankName = $_POST['bankName']; 
-//  $accountType = $_POST['accountType']; 
-//  $documents = $_POST['documents']; 
-//  $idFont = $_POST['idFont']; 
-//  $idback = $_POST['idback']; 
+$category_id = $_POST['category_id'];
+$partner_id = $_POST['partner_id'];
+$name = $_POST['name'];
+$short_description = $_POST['short_description'];
+$full_description = $_POST['full_description'];
+$cost = $_POST['cost'];
+$state = $_POST['state'];
+$photo = $_POST['photo'];
+$age = $_POST ['age']; 
+ $accountNumber = $_POST['accountNumber']; 
+ $bankName = $_POST['bankName']; 
+ $accountType = $_POST['accountType']; 
 
 
  // التحقق من الحقول المطلوبة
@@ -115,63 +113,6 @@ if (!Validator::number($_POST['cost'] ?? 0, 1, 10000000)) {
 //     header('Location: /charity_campaigns/create');
 //     exit();
 // }
-
-//  //  معالجة الملفات المرفوعة
-// $uploadDir = __DIR__ . 'views\media\images';
-// if (!is_dir($uploadDir)) {
-//     mkdir($uploadDir, 0755, true);
-// }
-
-// // معالجة المستندات الداعمة
-// $documents = [];
-// foreach ($_FILES['documents']['tmp_name'] as $key => $tmpName) {
-//     $fileName = basename($_FILES['documents']['name'][$key]);
-//     move_uploaded_file($tmpName, $uploadDir . $fileName);
-//     $documents[] = $fileName;
-// }
-
-// // معالجة صور البطاقة
-// $idFront = basename($_FILES['idFront']['name']);
-// move_uploaded_file($_FILES['idFront']['tmp_name'], $uploadDir . $idFront);
-
-// $idBack = basename($_FILES['idBack']['name']);
-// move_uploaded_file($_FILES['idBack']['tmp_name'], $uploadDir . $idBack);
-
-
-// // حفظ صور البطاقة
-// // $idFrontName = uniqid() . '_' . basename($_FILES['idFront']['name']);
-// // move_uploaded_file($_FILES['idFront']['tmp_name'], $uploadPath . $idFrontName);
-// // 
-// // $idBackName = uniqid() . '_' . basename($_FILES['idBack']['name']);
-// // move_uploaded_file($_FILES['idBack']['tmp_name'], $uploadPath . $idBackName);
-
-
-// $allowedTypes = ['image/jpeg', 'application/pdf'];
-// if (!in_array($_FILES['idFront']['type'], $allowedTypes)) {
-//     $errors['idFront'] = 'نوع الملف غير مسموح به';
-// }
-
-// if ($_FILES['idFront']['size'] > 2 * 1024 * 1024) {
-//     $errors['idFront'] = 'حجم الملف يتجاوز 2MB';
-// }
-
-//     $_SESSION['success'] = "تم تقديم الطلب بنجاح";
-//     header('Location: /charity_campaigns');
-//     exit();
-
-// } catch (PDOException $e) {
-//     error_log($e->getMessage());
-//     $_SESSION['error'] = "حدث خطأ أثناء حفظ البيانات";
-//     header('Location: /charity_campaigns/create');
-//     exit();
-// }
-
-// //  إذا كان هناك أخطاء، عرضها
-// if (!empty($errors)) {
-//     require "views/pages/charity_campaigns/create_view.php";
-//     die();
-// }
-
 
 try {
     $db->query(
@@ -211,6 +152,12 @@ try {
             'street' => htmlspecialchars($_POST['street'])
         ]
     );
+
+    $_SESSION['success'] = "تم تقديم الطلب بنجاح";
+    header('Location: /charity_campaigns');
+    exit();
+
+    
 } catch (PDOException $e) {
     error_log($e->getMessage());
     $_SESSION['error'] = "حدث خطأ أثناء حفظ البعانات";
