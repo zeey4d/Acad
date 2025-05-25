a04e34e14a4a0222b9ae3f6221018167d5ab3019
Ziad <fisal191919@gmail.com>
2025-05-26 00:34:31 +0300

    U

--- views/research/show_view.php
+++ views/research/show_view.php
@@ -71,7 +71,7 @@
                         
                         <div class="research-actions-large">
                             <a href="#" class="btn btn-large download-btn"><i class="fas fa-download"></i> تحميل البحث</a>
-                            <a href="#" class="btn btn-large cite-btn"><i class="fas fa-quote-right"></i> كيفية الاستشهاد</a>
+                            <a href="#" class="btn btn-large cite-btn"><i class="fas fa-quote-right"></i> قراءةالبحث </a>
                             <button class="btn-icon save-research" title="حفظ في المفضلة"><i class="far fa-bookmark"></i></button>
                             <button class="btn-icon share-research" title="مشاركة"><i class="fas fa-share-alt"></i></button>
                         </div>
